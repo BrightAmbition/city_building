4a1271430d8b7ad94b9f991d031ab04971df9b80
BrightAmbition <sse.apple1234@gmail.com>
2023-11-03 13:45:59 -0700

    almost_completed

--- src/game/scenes/Board.js
+++ src/game/scenes/Board.js
@@ -1,9 +1,16 @@
 import Phaser, { Scene } from "phaser";
 
 import ISOMETRIC_GRID from "../../assests/FfT_Grid_L2.png";
-import HOUSING from "../../assests/FfT_Block-Housing.svg";
-import HOSPITAL from "../../assests/FfT_Block_Hospital.svg";
-import GOLFCOURSE from "../../assests/FfT_Block_GolfCourse.svg";
+import HOUSING_1HA from "../../assests/Buildings/FfT_Housing_1ha.svg";
+import GOLFCOURSE_2HA from "../../assests/Buildings/FfT_GolfCourse_2ha.svg";
+import GROCERYSTORE_1HA from "../../assests/Buildings/FfT_Groceries_1ha.svg";
+import HOSPITAL_4HA from "../../assests/Buildings/FfT_Hospital_4ha.svg";
+import SCHOOL_4FT from "../../assests/Buildings/FfT_School_4ft.svg";
+import HOUSING from "../../assests/FfT_Housing_1ha.svg";
+import HOSPITAL from "../../assests/FfT_Hospital_4ha.svg";
+import GOLFCOURSE from "../../assests/FfT_GolfCourse_2ha.svg";
+import GROCERYSTORE from "../../assests/FfT_GroceryStore_1ha.svg";
+import SCHOOL from "../../assests/FfT_School_icon.svg";
 import CHEVRON_DOWN from "../../assests/FfT_Chevron_Down.svg";
 import CHEVRON_UP from "../../assests/FfT_Chevron_Up.svg";
 import TITLE from "../../assests/FfT_Title.svg";
@@ -19,16 +26,33 @@ import SelectMenu from "../components/SelectMenu";
 const DEVICEHEIGHT = window.innerHeight;
 const DEVICEWIDTH = window.innerWidth;
 
-const xspacing = 100;
-const yspacing = 54;
+const FIRSTBUILDINGX = 163;
+const FIRSTBUILDINGY = 463;
 
-const offsetX = 0;
-const offsetY = 22;
-const firstBuildingX = DEVICEWIDTH / 2;
-const firstBuildingY = DEVICEHEIGHT / 2;
+const DELTAXDELTAX = 100;
+const DELTAXDELTAY = -58;
+const DELTAYDELTAX = 100;
+const DELTAYDELTAY = 58;
 
-const BUILDINGOFFSET = 90;
-const BUILDINGSPACING = 200;
+const BUILDINGOFFSET = 200;
+const BUILDINGSPACING = 230;
+
+const MAXDISTANCE = Math.sqrt(100 * 100 + 58 * 58);
+
+const MAP = [
+  [1, 4],
+  [0, 5],
+  [-1, 6],
+  [-1, 7],
+  [0, 7],
+  [1, 8],
+  [2, 8],
+  [3, 8],
+  [4, 7],
+  [5, 6],
+];
+
+let position, offsetX, offsetY;
 
 class Board extends Scene {
   constructor(props) {
@@ -38,16 +62,104 @@ class Board extends Scene {
   }
 
   getPosition(row, col) {
-    const xPosition = firstBuildingX + (row + col - 2) * xspacing + offsetX;
-    const yPosition = firstBuildingY + (row - col) * yspacing + offsetY;
+    const x =
+      FIRSTBUILDINGX + (row - 1) * DELTAXDELTAX + (col - 1) * DELTAYDELTAX;
+    const y =
+      FIRSTBUILDINGY + (row - 1) * DELTAYDELTAY + (col - 1) * DELTAXDELTAY;
 
-    return { xPosition, yPosition };
+    return { x, y };
   }
 
-  initBuilding(index) {
-    if (index % 3 == 0) this.dragdropBuilding(index, "golfcourse");
-    if (index % 3 == 1) this.dragdropBuilding(index, "house");
-    if (index % 3 == 2) this.dragdropBuilding(index, "hospital");
+  initBuildings() {
+    this.input.on(
+      "dragstart",
+      (pointer, gameObject) => {
+        gameObject.offsetX =
+          pointer.x - gameObject.getWorldTransformMatrix().tx;
+        gameObject.offsetY =
+          pointer.y - gameObject.getWorldTransformMatrix().ty;
+      },
+      this
+    );
+
+    this.input.on("drag", (pointer, gameObject, dragX, dragY) => {
+      gameObject.x = dragX;
+      gameObject.y = dragY;
+      this.input.mouse.disableContextMenu = true;
+      this.game.canvas.style.cursor = "none";
+    });
+
+    this.input.on("dragend", (pointer, gameObject) => {
+      gameObject.x = BUILDINGOFFSET + gameObject.index * BUILDINGSPACING;
+      gameObject.y = 190;
+
+      this.input.mouse.disableContextMenu = false;
+      this.game.canvas.style.cursor = "default";
+
+      let index = this.getItemIndex(
+        pointer.x - gameObject.offsetX,
+        pointer.y - gameObject.offsetY
+      );
+
+      let item;
+      if (gameObject.index === 1) {
+        item = this.add
+          .image(
+            position[index.row][index.col].x + 5,
+            position[index.row][index.col].y,
+            "golfcourse_2ha"
+          )
+          .setOrigin(0, 1)
+          .setDisplaySize(286, 205)
+          .setDepth(index.row * 10 + 10 - index.col);
+      } else if (gameObject.index === 2) {
+        item = this.add
+          .image(
+            position[index.row][index.col].x,
+            position[index.row][index.col].y,
+            "house_1ha"
+          )
+          .setOrigin(0, 1)
+          .setDepth(index.row * 10 + 10 - index.col);
+      } else if (gameObject.index === 3) {
+        item = this.add
+          .image(
+            position[index.row][index.col].x - 95,
+            position[index.row][index.col].y,
+            "hospital_4ha"
+          )
+          .setOrigin(0, 1)
+          .setScale(2.2)
+          .setDepth(index.row * 10 + 10 - index.col);
+      } else if (gameObject.index === 4) {
+        item = this.add
+          .image(
+            position[index.row][index.col].x + 9,
+            position[index.row][index.col].y,
+            "grocerystore_1ha"
+          )
+          .setOrigin(0, 1)
+          .setDepth(index.row * 10 + 10 - index.col);
+      } else {
+        item = this.add
+          .image(
+            position[index.row][index.col].x - 105,
+            position[index.row][index.col].y,
+            "school_4ft"
+          )
+          .setOrigin(0, 1)
+          .setScale(2.2)
+          .setDepth(index.row * 10 + 10 - index.col);
+      }
+
+      item.setInteractive();
+      this.input.setDraggable(item);
+    });
+    this.dragdropBuilding(1, "golfcourse");
+    this.dragdropBuilding(2, "house");
+    this.dragdropBuilding(3, "hospital");
+    this.dragdropBuilding(4, "grocerystore");
+    this.dragdropBuilding(5, "school");
   }
 
   dragdropBuilding(index, buildingName) {
@@ -60,7 +172,10 @@ class Board extends Scene {
       .setOrigin(0, 1);
     this.chevron_down.add(img1);
 
+    img1.index = index;
+
     img1.setInteractive();
+
     this.input.setDraggable(img1);
 
     img1.on("dragstart", () => {
@@ -73,10 +188,11 @@ class Board extends Scene {
 
     let subDescription = null,
       description = null;
+
     if (buildingName === "golfcourse") {
       subDescription = this.add
         .text(
-          BUILDINGOFFSET + index * BUILDINGSPACING + 90,
+          BUILDINGOFFSET + index * BUILDINGSPACING + 80,
           200,
           "Hole of Golf (2ha)",
           {
@@ -85,49 +201,198 @@ class Board extends Scene {
             color: "#9fd771",
           }
         )
-        .setOrigin(0.5, 0.5);
+        .setOrigin(0.5, 0.5)
+        .setAlpha(0);
+      this.chevron_down.add(subDescription);
+
+      description = this.add
+        .text(BUILDINGOFFSET + index * BUILDINGSPACING + 80, 225, "Golf", {
+          fontFamily: "Arial",
+          fontSize: "20px",
+          color: "#9fd771",
+        })
+        .setOrigin(0.5, 0.5)
+        .setAlpha(0);
+
+      this.chevron_down.add(description);
+    }
+
+    if (buildingName === "house") {
+      subDescription = this.add
+        .text(
+          BUILDINGOFFSET + index * BUILDINGSPACING + 70,
+          200,
+          "Housing (1ha)",
+          {
+            fontFamily: "Arial",
+            fontSize: "12px",
+            color: "#9fd771",
+          }
+        )
+        .setOrigin(0.5, 0.5)
+        .setAlpha(0);
+      this.chevron_down.add(subDescription);
+
+      description = this.add
+        .text(BUILDINGOFFSET + index * BUILDINGSPACING + 70, 225, "Housing", {
+          fontFamily: "Arial",
+          fontSize: "20px",
+          color: "#9fd771",
+        })
+        .setOrigin(0.5, 0.5)
+        .setAlpha(0);
+
+      this.chevron_down.add(description);
+    }
+
+    if (buildingName === "hospital") {
+      subDescription = this.add
+        .text(
+          BUILDINGOFFSET + index * BUILDINGSPACING + 70,
+          200,
+          "Hospital (4ha)",
+          {
+            fontFamily: "Arial",
+            fontSize: "12px",
+            color: "#9fd771",
+          }
+        )
+        .setOrigin(0.5, 0.5)
+        .setAlpha(0);
       this.chevron_down.add(subDescription);
 
       description = this.add
         .text(
-          BUILDINGOFFSET + index * BUILDINGSPACING + 90,
+          BUILDINGOFFSET + index * BUILDINGSPACING + 70,
           225,
-          "Golf Course",
+          "Public Service",
           {
             fontFamily: "Arial",
             fontSize: "20px",
             color: "#9fd771",
           }
         )
-        .setOrigin(0.5, 0.5);
+        .setOrigin(0.5, 0.5)
+        .setAlpha(0);
 
       this.chevron_down.add(description);
     }
 
-    this.input.on("dragstart", (pointer, gameObject) => {}, this);
+    if (buildingName === "grocerystore") {
+      subDescription = this.add
+        .text(
+          BUILDINGOFFSET + index * BUILDINGSPACING + 70,
+          200,
+          "Grocery Store (1ha)",
+          {
+            fontFamily: "Arial",
+            fontSize: "12px",
+            color: "#9fd771",
+          }
+        )
+        .setOrigin(0.5, 0.5)
+        .setAlpha(0);
+      this.chevron_down.add(subDescription);
 
-    this.input.on("drag", (pointer, gameObject, dragX, dragY) => {
-      gameObject.x = dragX;
-      gameObject.y = dragY;
-      this.input.mouse.disableContextMenu = true;
-      this.game.canvas.style.cursor = "none";
+      description = this.add
+        .text(BUILDINGOFFSET + index * BUILDINGSPACING + 70, 225, "Shopping", {
+          fontFamily: "Arial",
+          fontSize: "20px",
+          color: "#9fd771",
+        })
+        .setOrigin(0.5, 0.5)
+        .setAlpha(0);
+
+      this.chevron_down.add(description);
+    }
+
+    if (buildingName === "school") {
+      subDescription = this.add
+        .text(
+          BUILDINGOFFSET + index * BUILDINGSPACING + 80,
+          200,
+          "School (4ft)",
+          {
+            fontFamily: "Arial",
+            fontSize: "12px",
+            color: "#9fd771",
+          }
+        )
+        .setOrigin(0.5, 0.5)
+        .setAlpha(0);
+      this.chevron_down.add(subDescription);
+
+      description = this.add
+        .text(
+          BUILDINGOFFSET + index * BUILDINGSPACING + 80,
+          225,
+          "Public Service",
+          {
+            fontFamily: "Arial",
+            fontSize: "20px",
+            color: "#9fd771",
+          }
+        )
+        .setOrigin(0.5, 0.5)
+        .setAlpha(0);
+
+      this.chevron_down.add(description);
+    }
+
+    img1.on("pointerover", () => {
+      description.setAlpha(1);
+      subDescription.setAlpha(1);
     });
 
-    this.input.on("dragend", (pointer, gameObject) => {
-      img1.x = BUILDINGOFFSET + index * BUILDINGSPACING;
-      img1.y = 190;
-
-      this.input.mouse.disableContextMenu = false;
-      this.game.canvas.style.cursor = "default";
-      console.log(gameObject.x + ", " + gameObject.y);
+    img1.on("pointerout", () => {
+      description.setAlpha(0);
+      subDescription.setAlpha(0);
     });
+  }
+
+  getItemIndex(xPos, yPos) {
+    let row = -1;
+    let col = -1;
+
+    console.log(xPos, yPos);
+
+    var min = Math.sqrt(
+      (position[0][2].x - xPos) * (position[0][2].x - xPos) +
+        (position[0][2].y - yPos) * (position[0][2].y - yPos)
+    );
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (position[i][j] === null) continue;
+
+        var temp = Math.sqrt(
+          (position[i][j].x - xPos) * (position[i][j].x - xPos) +
+            (position[i][j].y - yPos) * (position[i][j].y - yPos)
+        );
+
+        if (temp > MAXDISTANCE) continue;
+
+        if (temp < min) {
+          min = temp;
+          row = i;
+          col = j;
+        }
+      }
+    }
+
+    return { row, col };
   }
 
   preload() {
     this.load.image("grid", ISOMETRIC_GRID);
+    this.load.svg("house_1ha", HOUSING_1HA);
+    this.load.svg("golfcourse_2ha", GOLFCOURSE_2HA);
+    this.load.image("grocerystore_1ha", GROCERYSTORE_1HA);
+    this.load.image("hospital_4ha", HOSPITAL_4HA);
+    this.load.image("school_4ft", SCHOOL_4FT);
     this.load.image("house", HOUSING);
     this.load.image("hospital", HOSPITAL);
     this.load.image("golfcourse", GOLFCOURSE);
+    this.load.image("grocerystore", GROCERYSTORE);
     this.load.image("chevron_down", CHEVRON_DOWN);
     this.load.image("chevron_up", CHEVRON_UP);
     this.load.image("title", TITLE);
@@ -136,9 +401,26 @@ class Board extends Scene {
     this.load.image("dropdown_link", DROPDOWN_LINK);
     this.load.image("hamberger_menu", HAMBERGER_MENU);
     this.load.image("overlay", OVERLAY);
+    this.load.image("school", SCHOOL);
     // this.load.text("myFont", "../../assets/PPMori-Book.ttf");
   }
-  init() {}
+  init() {
+    let t = [];
+    for (let i = 0; i < 10; i++) {
+      let t1 = [];
+      for (let j = 0; j < 10; j++) {
+        if (j > MAP[i][0] && j < MAP[i][1] + 2) {
+          t1.push(this.getPosition(i + 1, j - 1));
+        } else {
+          t1.push(null);
+        }
+      }
+      t.push(t1);
+    }
+    position = t;
+
+    console.log(position);
+  }
   create() {
     this.chevron_down = this.add.container(0, DEVICEHEIGHT - 240).setDepth(1);
     this.chevron_up = this.add.container(0, DEVICEHEIGHT - 80).setDepth(1);
@@ -172,30 +454,21 @@ class Board extends Scene {
       .image(window.innerWidth / 2, window.innerHeight / 2 - 40, "grid")
       .setOrigin(0.5, 0.5);
 
-    this.add
-      .image(
-        this.getPosition(1, 2).xPosition,
-        this.getPosition(1, 2).yPosition,
-        "house"
-      )
-      .setOrigin(0, 1)
-      .setScale(1.3);
+    // this.add
+    //   .image(
+    //     this.getPosition(1, 2).xPosition,
+    //     this.getPosition(1, 2).yPosition,
+    //     "house"
+    //   )
+    //   .setOrigin(0, 1);
 
-    this.add
-      .image(
-        this.getPosition(1, 1).xPosition,
-        this.getPosition(1, 1).yPosition,
-        "golfcourse"
-      )
-      .setOrigin(0, 1);
-
-    this.add
-      .image(
-        this.getPosition(2, 0).xPosition,
-        this.getPosition(2, 0).yPosition,
-        "hospital"
-      )
-      .setOrigin(0, 1);
+    // this.add
+    //   .image(
+    //     this.getPosition(2, 0).xPosition,
+    //     this.getPosition(2, 0).yPosition,
+    //     "hospital"
+    //   )
+    //   .setOrigin(0, 1);
 
     // Chevron Down Container
     // Chevron Down
@@ -222,9 +495,7 @@ class Board extends Scene {
     );
 
     // Building items
-    for (var i = 0; i < 9; i++) {
-      this.initBuilding(i);
-    }
+    this.initBuildings();
 
     //Add Buttons
     const fairway_btn = new Button(
